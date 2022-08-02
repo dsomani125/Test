b12967393df11beb31b0f6a25dddffc3541ef9a1
dsomani125 <45123778+dsomani125@users.noreply.github.com>
2022-08-02 12:01:57 +0530

    Create articleData.ts

---
+++ articleData.ts
@@ -0,0 +1,254 @@
+[
+	{
+		category: 'payment',
+		articleId: '09dd52e9-ea16-4c13-92d0-dd98d85e2539',
+		layout: 1,
+		articleTitle: 'Tagih & Bayar Ke Pelanggan Cuma Perlu 1 Aplikasi',
+		updatedAt: '2022-07-04T08:33:40Z',
+		shareAssets: [
+			{
+				id: 1,
+				label: 'Whatsapp',
+				img: '/Icons/whatsapp-share.svg',
+				deeplink: 'http: whatsapp',
+				type: 'share',
+			},
+			{
+				id: 2,
+				label: 'Link',
+				img: '/Icons/link-share.svg',
+				deeplink: 'http: link',
+				type: 'copy',
+			},
+		],
+		articleSubTitle:
+			'<div> Keuntungan Tambahan <span>Tagih</span> dan <span>Bayar</span><span>Uang</span> Pakai BukuWarung</div>',
+		articleBenifits: [
+			{
+				id: 1,
+				title: 'Praktis, Gak Pake Lama',
+				description:
+					'Bayar atau Tagih supplier dari mana aja tinggal pencet di aplikasi!',
+				image: '/Icons/paym-benifit-1.svg',
+			},
+			{
+				id: 2,
+				title: 'Aman dan Terpercaya',
+				description: 'Didukung oleh Xendit dan OY! Berlisensi Bank Indonesia.',
+				image: '/Icons/paym-benifit-2.svg',
+			},
+			{
+				id: 3,
+				title: 'Metode Pembayaran Lengkap',
+				description:
+					'Bayar dan tagih lewat berbagai Bank nasional, retail, dan dompet digital.',
+				image: '/Icons/paym-benifit-3.svg',
+			},
+			{
+				id: 4,
+				title: 'Dapat Catatan Keuangan Otomatis',
+				description:
+					'Riwayat pembayaran dan keuntungan akan tercatat otomatis di aplikasi. ',
+				image: '/Icons/paym-benifit-4.svg',
+			},
+		],
+		tabs: [
+			{
+				key: 0,
+				label: 'Tutorial Tagih',
+				title: 'Cara Mudah Tagih ke Pelanggan Pakai BukuWarung',
+				carouselData: [
+					{
+						serial: 1,
+						title: 'Klik Menu Tagih',
+						description:
+							'Klik menu Tagih pada bagian atas halaman Home BukuWarung',
+						image: `${imageBaseURL}/education-hub/paym-assets/tagih/tagih-1.webp`,
+					},
+					{
+						serial: 2,
+						title: 'Masukkan Jumlah Tagihan',
+						description:
+							'Masukkan jumlah tagihan pelanggan dan sertakan catatan transaksi',
+						image: `${imageBaseURL}/education-hub/paym-assets/tagih/tagih-2.webp`,
+					},
+					{
+						serial: 3,
+						title: 'Pilih Pelanggan',
+						description:
+							'Pilih pelanggan dari kontak atau tambahkan nama serta nomor HP pelanggan',
+						image: `${imageBaseURL}/education-hub/paym-assets/tagih/tagih-3.webp`,
+					},
+					{
+						serial: 4,
+						title: 'Kirim Tagihan',
+						description:
+							'Masukkan nomor rekening dan pastikan nomor rekening sudah benar lalu klik Kirim Tagihan',
+						image: `${imageBaseURL}/education-hub/paym-assets/tagih/tagih-4.webp`,
+					},
+				],
+			},
+			{
+				key: 1,
+				label: 'Tutorial Bayar',
+				title: 'Cara Mudah Bayar Uang Pakai BukuWarung',
+				carouselData: [
+					{
+						serial: 1,
+						title: 'Klik Menu Bayar',
+						description:
+							'Klik menu Bayar pada bagian atas halaman Home BukuWarung',
+						image: `${imageBaseURL}/education-hub/paym-assets/bayar/bayar-1.webp`,
+					},
+					{
+						serial: 2,
+						title: 'Masukkan Rekening Penerima & Jumlah Pembayaran',
+						description: 'Masukkan Rekening Penerima & Jumlah Pembayaran',
+						image: `${imageBaseURL}/education-hub/paym-assets/bayar/bayar-2.webp`,
+					},
+					{
+						serial: 3,
+						title: 'Pilih Pelanggan',
+						description:
+							'Pilih pelanggan dari kontak atau tambahkan nama serta nomor HP pelanggan',
+						image: `${imageBaseURL}/education-hub/paym-assets/bayar/bayar-3.webp`,
+					},
+					{
+						serial: 4,
+						title: 'Klik Bayar',
+						description: 'Jika semua langkah sudah dilakukan, klik Bayar',
+						image: `${imageBaseURL}/education-hub/paym-assets/bayar/bayar-4.webp`,
+					},
+				],
+			},
+		],
+		ctaTitle: 'Coba Sekarang',
+		deeplink: 'com.bukuwarung.payments.checkout.PaymentCheckoutActivity',
+	},
+	{
+		category: 'qris',
+		articleId: '2b5431ca-1080-4753-9d5c-6d1ae0c76d24',
+		layout: 2,
+		articleTitle: 'Daftar QRIS BukuWarung, Usaha jadi Makin Untung',
+		updatedAt: '2022-07-04T08:33:40Z',
+		shareAssets: [
+			{
+				id: 1,
+				label: 'Whatsapp',
+				img: '/Icons/whatsapp-share.svg',
+				deeplink: 'http: whatsapp',
+				type: 'share',
+			},
+			{
+				id: 2,
+				label: 'Link',
+				img: '/Icons/link-share.svg',
+				deeplink: 'http: link',
+				type: 'copy',
+			},
+		],
+		articleSubTitle: '<div> Kenapa <span>QRIS</span> BukuWarung?</div>',
+		productBenifits: [
+			{
+				id: 1,
+				title: 'Praktis, Gak Pake Lama',
+				description:
+					'Bayar atau Tagih supplier dari mana aja tinggal pencet di aplikasi!',
+				image: '/Icons/paym-benifit-1.svg',
+			},
+			{
+				id: 2,
+				title: 'Aman dan Terpercaya',
+				description: 'Didukung oleh Xendit dan OY! Berlisensi Bank Indonesia.',
+				image: '/Icons/paym-benifit-2.svg',
+			},
+		],
+		articleBenifits: [
+			{
+				id: 1,
+				title: 'Praktis, Gak Pake Lama',
+				description:
+					'Bayar atau Tagih supplier dari mana aja tinggal pencet di aplikasi!',
+				image: '/Icons/paym-benifit-1.svg',
+			},
+			{
+				id: 2,
+				title: 'Aman dan Terpercaya',
+				description: 'Didukung oleh Xendit dan OY! Berlisensi Bank Indonesia.',
+				image: '/Icons/paym-benifit-2.svg',
+			},
+			{
+				id: 3,
+				title: 'Metode Pembayaran Lengkap',
+				description:
+					'Bayar dan tagih lewat berbagai Bank nasional, retail, dan dompet digital.',
+				image: '/Icons/paym-benifit-3.svg',
+			},
+		],
+		tabs: [
+			{
+				key: 0,
+				label: 'Cara Daftar QRIS BukuWarung',
+				title: 'Cara Daftar Kode QRIS BukuWarung',
+				videoUrl: 'https://www.youtube.com/embed/Yy3anSO0foA',
+				thumbnail: `${imageBaseURL}/education-hub/qris/thumbnail/qris-thumbnail-1.webp`,
+				howToUseTitle: 'Cara Daftar Kode QRIS BukuWarung',
+				howToUseVideoUrl: 'https://www.youtube.com/embed/Yy3anSO0foA',
+				aboutProductTitle: 'Tentang QRIS',
+				aboutProductDescription: [
+					'QRIS adalah standarisasi pembayaran melalui kode QR yang dikeluarkan langsung oleh Bank Indonesia. Saat ini BukuWarung telah terintegrasi dengan Quick Response Code Indonesian Standard (QRIS) yang bisa digunakan untuk menerima pembayaran dari berbagai bank dan e-wallet.',
+					'Cara daftar QRIS BukuWarung cukup praktis. Buka aplikasi BukuWarung, lalu pilih icon QRIS yang berada paling atas, lalu tinggal mengikuti instruksi saja dan selamat Juragan sudah bisa menerima pembayaran QRIS sekarang.',
+				],
+				TnC: [
+					'Di aplikasi BukuWarung, klik banner atau tombol QRIS di laman Home atau Pembayaran',
+					'Klik Ajukan QRIS Sekarang',
+					'Ambil foto KTP & Selfie dengan KTP (jika belum Akun Premium)',
+					'Lengkapi Data Usaha & Alamat Usaha',
+				],
+			},
+			{
+				key: 1,
+				label: 'Cara Download Kode QRIS BukuWarung',
+				title: 'Cara Mudah Bayar Uang Pakai BukuWarung',
+				videoUrl: 'https://www.youtube.com/embed/Yy3anSO0foA',
+				thumbnail: `${imageBaseURL}/education-hub/qris/thumbnail/qris-thumbnail-1.webp`,
+				howToUseTitle: 'Cara Daftar Kode QRIS BukuWarung',
+				howToUseVideoUrl: 'https://www.youtube.com/embed/Yy3anSO0foA',
+				aboutProductTitle: 'Tentang QRIS',
+				aboutProductDescription: [
+					'QRIS adalah standarisasi pembayaran melalui kode QR yang dikeluarkan langsung oleh Bank Indonesia. Saat ini BukuWarung telah terintegrasi dengan Quick Response Code Indonesian Standard (QRIS) yang bisa digunakan untuk menerima pembayaran dari berbagai bank dan e-wallet.',
+					'Cara daftar QRIS BukuWarung cukup praktis. Buka aplikasi BukuWarung, lalu pilih icon QRIS yang berada paling atas, lalu tinggal mengikuti instruksi saja dan selamat Juragan sudah bisa menerima pembayaran QRIS sekarang.',
+				],
+				TnC: [
+					'Di aplikasi BukuWarung, klik banner atau tombol QRIS di laman Home atau Pembayaran',
+					'Klik Ajukan QRIS Sekarang',
+					'Ambil foto KTP & Selfie dengan KTP (jika belum Akun Premium)',
+					'Lengkapi Data Usaha & Alamat Usaha',
+				],
+			},
+			{
+				key: 2,
+				label: 'Cara Menerima Pembayaran lewat QRIS BukuWarung',
+				title: 'Cara Menerima Pembayaran Lewat QRIS BukuWarung',
+				videoUrl: 'https://www.youtube.com/embed/Yy3anSO0foA',
+				thumbnail: `${imageBaseURL}/education-hub/qris/thumbnail/qris-thumbnail-1.webp`,
+				howToUseTitle: 'Cara Daftar Kode QRIS BukuWarung',
+				howToUseVideoUrl: 'https://www.youtube.com/embed/Yy3anSO0foA',
+				aboutProductTitle: 'Tentang QRIS',
+				aboutProductDescription: [
+					'QRIS adalah standarisasi pembayaran melalui kode QR yang dikeluarkan langsung oleh Bank Indonesia. Saat ini BukuWarung telah terintegrasi dengan Quick Response Code Indonesian Standard (QRIS) yang bisa digunakan untuk menerima pembayaran dari berbagai bank dan e-wallet.',
+					'Cara daftar QRIS BukuWarung cukup praktis. Buka aplikasi BukuWarung, lalu pilih icon QRIS yang berada paling atas, lalu tinggal mengikuti instruksi saja dan selamat Juragan sudah bisa menerima pembayaran QRIS sekarang.',
+				],
+				TnC: [
+					'Di aplikasi BukuWarung, klik banner atau tombol QRIS di laman Home atau Pembayaran',
+					'Klik Ajukan QRIS Sekarang',
+					'Ambil foto KTP & Selfie dengan KTP (jika belum Akun Premium)',
+					'Lengkapi Data Usaha & Alamat Usaha',
+					'Lengkapi Data Usaha & Alamat Usaha',
+				],
+			},
+		],
+		ctaTitle: 'Coba Sekarang',
+		deeplink: 'https://api-dev.bukuwarung.com/payments-mweb/qris/start',
+	},
+];
